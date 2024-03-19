eb646c2832e33afd96799b88ef564bd95605fd0a
Walker Milhoan <walker@milhoandesign.com>
2024-03-19 06:00:01 -0600

    Added btn click event

--- main.js
+++ main.js
@@ -11,3 +11,19 @@ navLinks.forEach(function(link) {
         alert("You clicked on " + this.textContent);
     });
 });
+
+// Clickable Button Event
+document.addEventListener("DOMContentLoaded", () => {
+    function createParagraph() {
+      const para = document.createElement("p");
+      para.textContent = "You clicked the button!";
+      document.body.appendChild(para);
+    }
+  
+    const buttons = document.querySelectorAll("button");
+  
+    for (const button of buttons) {
+      button.addEventListener("click", createParagraph);
+    }
+  });
+  
